b9e449b4e4c2e447a664d12259598ab805fa56d4
amitnandan <a.amitnandan@gmail.com>
2024-06-27 17:06:48 +0530

    added code for 2 db connection in single springboot

---
+++ TwoDatabase/src/main/java/org/amit/TwoDb.java
@@ -0,0 +1,12 @@
+package org.amit;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoDb {
+    public static void main(String[] args) {
+        SpringApplication.run(TwoDb.class , args);
+    }
+}
